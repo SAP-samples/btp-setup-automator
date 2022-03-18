de6d07e2afac48c709e0467c279f16e416d039e0
rui1610 <rui.nogueira1610@hotmail.com>
2022-03-18 07:47:22 +0100

    update ENV for git version

--- run.ps1
+++ run.ps1
@@ -9,4 +9,4 @@ Write-Host "Building the container image ..." -ForegroundColor green
 docker image build -t btp-setup-automator:latest -f "config/containerdefinitions/btp-setup-automator/Dockerfile"  .
 
 Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VSCode" -ForegroundColor green
-docker container run -e BTPSETUPAUTOMATOR_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name "btp-setup-automator" "btp-setup-automator" 
+docker container run -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name "btp-setup-automator" "btp-setup-automator" 
