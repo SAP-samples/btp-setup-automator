d8d4cc63790ce572c26d0f70c81287d9ee39fbf9
Rui Nogueira <rui1610@users.noreply.github.com>
2022-04-29 13:26:19 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -8,10 +8,10 @@ docker container rm  -f "btp-setup-automator"
 docker image     rmi -f "btp-setup-automator"
 
 echo %ESC%[32mBuilding the container image ...%ESC%[0m
-docker image     build -t btp-setup-automator:latest -f .\config\containerdefinitions\btp-setup-automator\Dockerfile .
+docker image     build --platform linux/amd64 -t btp-setup-automator:latest -f .\config\containerdefinitions\btp-setup-automator\Dockerfile .
 
 echo %ESC%[32mStart the container as 'btp-setup-automator' - Access possible e.g. via VSCode%ESC%[0m
-docker container run -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name btp-setup-automator btp-setup-automator
+docker container run --platform linux/amd64 -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name btp-setup-automator btp-setup-automator
 
 :setESC
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
