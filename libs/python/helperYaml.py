b0e50a361ff4b3efb4d5d5cd7e2de6f200ebe836
Christian Lechner <christian.lechner@sap.com>
2022-05-25 10:57:37 +0200

    restructuring of service creation 2

--- libs/python/helperYaml.py
+++ libs/python/helperYaml.py
@@ -2,9 +2,19 @@ import yaml
 from libs.python.helperJson import getJsonFromFile
 
 
-def build_service_instance_yaml_from_parameters():
+def build_service_instance_yaml_from_parameters(service):
 
     serviceInstanceTemplate = getJsonFromFile('libs/json/templates/K8s-SERVICE-INSTANCE.json')
+
+    serviceInstanceTemplate.metadata.name = service.instanceName
+    serviceInstanceTemplate.spec.serviceOfferingName = service.name
+    serviceInstanceTemplate.spec.servicePlanName = service.plan
+    serviceInstanceTemplate.spec.externalName = service.instanceName
+
+    if service.parameters is not None:
+        serviceInstanceTemplate.spec.parameters = service.parameters
+    elif service.serviceparameterfile is not None:
+        serviceInstanceTemplate.spec.parameters = getJsonFromFile(service.serviceparameterfile)       
 
     serviceInstanceYaml = yaml.dump(serviceInstanceTemplate, default_flow_style=False)
 
@@ -22,4 +32,5 @@ def build_service_binding_yaml_from_parameters():
 
 def store_yaml_to_disk(yamlFilePath, yamlContent):
     with open(yamlFilePath, 'w') as outfile:
-        outfile.write(yamlContent)
+        yaml.dump(yamlContent, outfile)
+    return True
