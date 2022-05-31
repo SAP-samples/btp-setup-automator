548abee2cd3739046cdab476762f78d01ff80fca
rui1610 <rui.nogueira1610@hotmail.com>
2022-05-31 18:46:39 +0200

    update folder names in code

--- libs/python/helperDocumentation.py
+++ libs/python/helperDocumentation.py
@@ -1,6 +1,6 @@
 from libs.python.helperJinja2 import renderTemplateWithJson
 from libs.python.helperJson import getJsonFromFile
-from libs.python.helperFolders import FOLDER_SCHEMA_LIBS, FOLDER_SCHEMA_TEMPLATES
+from libs.python.helperFolders import FOLDER_SCHEMA_LIBS, FOLDER_DOCS_TEMPLATES, FOLDER_DOCS_OUTPUT
 import logging
 
 log = logging.getLogger(__name__)
@@ -10,12 +10,12 @@ def updateDocumentation():
 
     data = getJsonFromFile(None, FOLDER_SCHEMA_LIBS + "btpsa-usecase.json")
     parameters = data.get("properties")
-    targetFilename = "./docs/PARAMETERS-SERVICES.md"
-    renderTemplateWithJson(FOLDER_SCHEMA_TEMPLATES, "PARAMETERS-SERVICES.md", targetFilename, parameters)
+    targetFilename = FOLDER_DOCS_OUTPUT + "/PARAMETERS-SERVICES.md"
+    renderTemplateWithJson(FOLDER_DOCS_TEMPLATES, "PARAMETERS-SERVICES.md", targetFilename, parameters)
     log.success("updated the documentation for use cases at >" + targetFilename + "<")
 
     data = getJsonFromFile(None, FOLDER_SCHEMA_LIBS + "btpsa-parameters.json")
     parameters = data.get("properties")
-    targetFilename = "./docs/PARAMETERS-BTPSA.md"
-    renderTemplateWithJson(FOLDER_SCHEMA_TEMPLATES, "PARAMETERS-BTPSA.md", targetFilename, parameters)
+    targetFilename = FOLDER_DOCS_OUTPUT + "/PARAMETERS-BTPSA.md"
+    renderTemplateWithJson(FOLDER_DOCS_TEMPLATES, "PARAMETERS-BTPSA.md", targetFilename, parameters)
     log.success("updated the documentation for btpsa parameters at >" + targetFilename + "<")
--- libs/python/helperFolders.py
+++ libs/python/helperFolders.py
@@ -1,2 +1,5 @@
 FOLDER_SCHEMA_TEMPLATES = "./config/jsonschemas/"
 FOLDER_SCHEMA_LIBS = "./libs/"
+
+FOLDER_DOCS_TEMPLATES = "./config/docs/"
+FOLDER_DOCS_OUTPUT = "./docs/"
