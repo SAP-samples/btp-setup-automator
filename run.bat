e6047fdd4daed22daff945b915f46d0af191cf1c
Christian Lechner <lechnerc77@users.noreply.github.com>
2022-05-31 09:21:03 +0200

    Adjustment of .bat and .ps1 files to Dockerfile location

--- run.bat
+++ run.bat
@@ -11,7 +11,7 @@ docker image     rmi -f "btp-setup-automator"
 if not "%1" == "RunFromRegistry" (
   
   echo %ESC%[32mBuilding the container image ...%ESC%[0m
-  docker image     build -t btp-setup-automator:latest -f .\config\containerdefinitions\btp-setup-automator\Dockerfile .
+  docker image     build -t btp-setup-automator:latest -f .\config\Dockerfile .
 
   echo %ESC%[32mStart the container as 'btp-setup-automator' - Access possible e.g. via VSCode%ESC%[0m
   docker container run -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name btp-setup-automator btp-setup-automator
--- run.ps1
+++ run.ps1
@@ -12,7 +12,7 @@ docker image     rmi -f "btp-setup-automator"
 if ( $RunFromRegistry -eq $False )
 {
     Write-Host "Building the container image ..." -ForegroundColor green
-    docker image build -t btp-setup-automator:latest -f "config/containerdefinitions/btp-setup-automator/Dockerfile"  .
+    docker image build -t btp-setup-automator:latest -f "config/Dockerfile"  .
 
     Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VSCode" -ForegroundColor green
     docker container run -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name "btp-setup-automator" "btp-setup-automator" 
