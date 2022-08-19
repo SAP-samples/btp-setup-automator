7190a94a586221e881d728bfc0349c1fd0fbf3a0
rui1610 <rui.nogueira1610@hotmail.com>
2022-08-19 09:26:29 +0200

    support manually maintained json schemas

--- libs/python/helperGenerator.py
+++ libs/python/helperGenerator.py
@@ -4,18 +4,53 @@ import requests
 import logging
 import json
 import jinja2
+import glob
+from pathlib import Path
 
 log = logging.getLogger(__name__)
+
 
 def fetchEntitledServiceList(mainDataJsonFile, datacenterFile):
     resultServices = getJsonFromFile(mainDataJsonFile)
     resultDCs = getJsonFromFile(datacenterFile)
     btpservicelist = resultServices["services"]
+    addManuallyMaintainedServiceSchema(btpservicelist)
 
     resultDCs = sorted(resultDCs, key=lambda d: (d['region'].lower()), reverse=False)
 
     thisResult = {"btpservicelist": btpservicelist, "datacenterslist": resultDCs}
     return thisResult
+
+
+# This function will add schema information to service plans
+# in case they are not provided in the fetched metadata
+# and manually maintained in the folder config/services-jsonschemas
+def addManuallyMaintainedServiceSchema(btpservicelist):
+    FOLDER_WITH_MANUAL_SCHEMAS = Path(__file__, "..", "..", "..", "config", "services-jsonschemas").resolve()
+
+    # first get the manually maintained json schemas
+    manuallyMaintainedSchemaFiles = []
+    for root, dirs, files in os.walk(FOLDER_WITH_MANUAL_SCHEMAS):
+        for file in files:
+            if file.endswith(".json"):
+                manuallyMaintainedSchemaFiles.append(os.path.join(root, file))
+
+    manuallyMaintainedSchemas = []
+    for thisFile in manuallyMaintainedSchemaFiles:
+        schemaInfo = getJsonFromFile(thisFile)
+        for thisSchemaInfo in schemaInfo:
+            manuallyMaintainedSchemas.append(thisSchemaInfo)
+
+    for serviceType in btpservicelist:
+        for service in serviceType.get("list"):
+            for plan in service.get("servicePlans"):
+                resultingSchemas = [thisSchema for thisSchema in manuallyMaintainedSchemas if thisSchema.get("name") == service.get("name") and thisSchema.get("plan") == plan.get("name")]
+                if resultingSchemas and len(resultingSchemas) > 0:
+                    if len(resultingSchemas) == 1:
+                        plan["schemas"] = resultingSchemas[0].get("schemas")
+                        resultingSchemas = resultingSchemas
+                    else:
+                        print("ERROR: Can't add multiple schema info to a service plan!")
 
 
 def renderTemplateWithJson(templateFilename, targetFilename, parameters):
