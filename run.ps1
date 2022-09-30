7e49f236620da21bcc50317da586d7e00dda63f7
DJ Adams <dj.adams@sap.com>
2022-09-30 12:17:20 +0100

    VSCode -> VS Code

--- run.ps1
+++ run.ps1
@@ -14,7 +14,7 @@ if ( $RunFromRegistry -eq $False )
     Write-Host "Building the container image ..." -ForegroundColor green
     docker image build -t btp-setup-automator:latest -f "config/Dockerfile"  .
 
-    Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VSCode" -ForegroundColor green
+    Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VS Code" -ForegroundColor green
     docker container run -e BTPSA_VERSION_GIT="$(git describe --long --tags  --always)" --rm  -it -d --name "btp-setup-automator" "btp-setup-automator" 
 }
 else
@@ -22,6 +22,6 @@ else
     Write-Host "Pulling container image from registry ..." -ForegroundColor green
     docker pull ghcr.io/sap-samples/btp-setup-automator:main 
 
-    Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VSCode" -ForegroundColor green
+    Write-Host "Start the container as 'btp-setup-automator' - Access possible e.g. via VS Code" -ForegroundColor green
     docker container run --rm  -it -d --name "btp-setup-automator" ghcr.io/sap-samples/btp-setup-automator:main 
 }
