59e16d5a27a847769212533c84d676f46d069474
frsap <felix.ruecker@sap.com>
2023-03-10 17:36:20 +0100

    feat: additional parameter files (#456)
    * feat: additional parameter files
    
    * chore: cleanup and merge
    
    * chore: typo & formatting

--- libs/python/btp_cli.py
+++ libs/python/btp_cli.py
@@ -724,6 +724,15 @@ class BTPUSECASE:
                         self.create_and_assign_quota_plan(environment)
 
                 elif environment.name == "kymaruntime":
+                    # Support load via dynamic parameter file as well
+                    if environment.parameters is None:
+                        # try via serviceparameter file
+                        command = f'cat "{environment.serviceparameterfile}"'
+                        message = "Read out environment parameter file"
+                        environment.parameters = runCommandAndGetJsonResult(
+                            self, command, "INFO", message
+                        )
+
                     kymaClusterName = environment.parameters["name"]
 
                     # Set Cluster region: the cluster region can be globally defined via the parameters file
